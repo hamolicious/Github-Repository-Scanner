9f99c933d53e34c7f8b3e5248a3df88b8adea862
hamolicious <kickfliptv8@gmail.com>
2022-08-22 09:41:19 +0100

    fixed exception when viewing archived repos

--- scan_github_repos_stats.py
+++ scan_github_repos_stats.py
@@ -4,17 +4,19 @@ from os import system ; clear = lambda : system('cls') ; clear()
 from table import Table, Colours, fg, reset
 
 #region login
-title = fg(Colours.title_colour) + """   _____ _ _   _           _        _____ _        _          _____
-  / ____(_) | | |         | |      / ____| |      | |        / ____|
- | |  __ _| |_| |__  _   _| |__   | (___ | |_ __ _| |_ ___  | (___   ___ __ _ _ __  _ __   ___ _ __
- | | |_ | | __| '_ \| | | | '_ \   \___ \| __/ _` | __/ __|  \___ \ / __/ _` | '_ \| '_ \ / _ \ '__|
- | |__| | | |_| | | | |_| | |_) |  ____) | || (_| | |_\__ \  ____) | (_| (_| | | | | | | |  __/ |
-  \_____|_|\__|_| |_|\__,_|_.__/  |_____/ \__\__,_|\__|___/ |_____/ \___\__,_|_| |_|_| |_|\___|_|   """ + reset()
+title = fg(Colours.title_colour) + \
+"""
+   ___ _ _   _           _       __ _        _         __
+  / _ (_) |_| |__  _   _| |__   / _\ |_ __ _| |_ ___  / _\ ___ __ _ _ __  _ __   ___ _ __
+ / /_\/ | __| '_ \| | | | '_ \  \ \| __/ _` | __/ __| \ \ / __/ _` | '_ \| '_ \ / _ \ '__|
+/ /_\\\\| | |_| | | | |_| | |_) | _\ \ || (_| | |_\__ \ _\ \ (_| (_| | | | | | | |  __/ |
+\____/|_|\__|_| |_|\__,_|_.__/  \__/\__\__,_|\__|___/ \__/\___\__,_|_| |_|_| |_|\___|_|
+""" + reset()
 
 print(title)
 user = 'hamolicious'
 with open('token.key', 'r') as file:
-    key = file.read()
+		key = file.read()
 git_hub = Github(key)
 #endregion
 
@@ -26,21 +28,24 @@ repos.sort(key=lambda elem: elem.name)
 #endregion
 
 def get_stats_data():
-    data = [['Repository Name', 'Stars', 'Views', 'Clones', 'Watchers', 'Forks']]
-    for repo in repos:
-        data.append([
-            repo.name,
-            repo.stargazers_count,
-            repo.get_views_traffic(per='week').get('count'),
-            repo.get_clones_traffic(per='week').get('count'),
-            repo.watchers_count,
-            repo.forks_count,
-        ])
+		data = [['Repository Name', 'Stars', 'Views', 'Clones', 'Watchers', 'Forks']]
+		for repo in repos:
+				if repo.archived:
+					continue
 
-    return data
+				data.append([
+					repo.name,
+					repo.stargazers_count,
+					repo.get_views_traffic(per='week').get('count'),
+					repo.get_clones_traffic(per='week').get('count'),
+					repo.watchers_count,
+					repo.forks_count,
+				])
+
+		return data
 
 data = get_stats_data()
-stats_table = Table(data, add_total_row=True, add_mean_row=True, add_mode_row=True)
+stats_table = Table(data, add_total_row=True, add_mean_row=True)
 stats_table.update()
 
 #region print info
